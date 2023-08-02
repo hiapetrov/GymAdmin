47587b9cc3792a94275b25de34eab5d0fb966dcc
Alexander <hi.apetrov@gmail.com>
2023-08-02 15:46:48 +0100

    server.js created and tested

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log('hello wolrd')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.get('/api/appointments/:id', (req, res) => {
+    const id = req.params.id
+    res.json({ message: `got milk ${id}`})
+})
+
+app.listen(port, () => console.log(`na maika ti putkata ${port}`))
