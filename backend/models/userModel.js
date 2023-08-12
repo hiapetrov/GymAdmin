30ea53286b035764a7c54665c19488fc32b05b0b
Alexander <hi.apetrov@gmail.com>
2023-08-12 16:24:55 +0100

    pass hash bug fixed

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -25,7 +25,6 @@ const userSchema = mongoose.Schema ({
 }
 )
 
-const User = mongoose.model('User', userSchema);
 
 userSchema.pre("save", async function (next) {
     if(!this.isModified('password')) {
@@ -36,5 +35,7 @@ userSchema.pre("save", async function (next) {
     this.password = await bcrypt.hash(this.password, salt)
     
 })
+const User = mongoose.model('User', userSchema);
+
 
 module.exports = User
