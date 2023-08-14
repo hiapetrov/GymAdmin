b5426967be193ff3f7e01bab608e3fd018d71ae2
Alexander <hi.apetrov@gmail.com>
2023-08-14 13:52:05 +0100

    protect bug fixed

--- backend/middleware/authHandler.js
+++ backend/middleware/authHandler.js
@@ -7,14 +7,14 @@ import User from '../models/userModel.js'
 
 const protect = asyncHandler( async (req, res, next) => {
     let token;
-    
+
     token = req.cookies.jwt
 
     if (token) {
         try {
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-            req.user = await User.findOne(decoded.userID).select("-password")
+            req.user = await User.findById(decoded.userID).select("-password")
 
             next()
         } catch (error) {
