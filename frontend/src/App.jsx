b79a667cb4d1e6461c11a33f186099cc14b0f543
Alexander <hi.apetrov@gmail.com>
2023-08-15 23:27:58 +0100

    App.jsx-container-created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
-// App.js
-import React, { useState } from 'react';
-import Header from './components/Header.jsx';
-import Story from './components/Story.jsx';
-import Login from './components/LoginForm.jsx';
-import SignUp from './components/SingupForm.jsx';
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from './components/Header';
 
 const App = () => {
-
   return (
     <>
+      <Header />
+      <ToastContainer />
+      <Container className='my-2'>
+        <Outlet />
+      </Container>
     </>
   );
 };
