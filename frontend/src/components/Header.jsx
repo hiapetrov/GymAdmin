ad2bf7153e3df4496c3bddfdd1756fcf612b7a46
Alexander <hi.apetrov@gmail.com>
2023-08-17 23:42:58 +0100

    Header-modified-copy-traversy

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,49 +1,69 @@
-// Header.js
-import React from 'react';
+// import { Navbar, Nav, Container, NavDropdown, Badge } from 'react-bootstrap';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
+import { LinkContainer } from 'react-router-bootstrap';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { useLogoutMutation } from '../slices/usersApiSlice';
+import { logout } from '../slices/authSlice';
 
-const Header = ({ setAuthState, authState }) => {
-  const handleHomeButtonClick = () => {
-    setAuthState(0);
-  }
+const Header = () => {
+  const { userInfo } = useSelector((state) => state.auth);
 
-  const handleLoginButtonClick = () => {
-    setAuthState(1);
-  }
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const handleSignUpButtonClick = () => {
-    setAuthState(2);
-  }
+  const [logoutApiCall] = useLogoutMutation();
 
-  const handleLogoutClick = () => {
-    setAuthState(0)
-  }
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate('/login');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <header className="header">
-      <div>
-        <img src="../src/assets/simple-logo.png" alt="Logo" className="logo" />
-      </div>
-      <div className="button-container">
-        {authState === 0 ? (
-          <>
-            <button className="button" onClick={handleLoginButtonClick}>
-              Login
-            </button>
-            <button className="button" onClick={handleSignUpButtonClick}>
-              Sign Up
-            </button>
-          </>
-        ) : (
-          <button className="button" onClick={handleHomeButtonClick}>
-            Go Back
-          </button>
-        )}
-        {authState === 3 && (
-          <>
-            {/* Display the logout button */}
-            <button onClick={handleLogout}>Logout</button>
-          </>
-        )}
-      </div>
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>MERN Auth</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              {userInfo ? (
+                <>
+                  <NavDropdown title={userInfo.name} id='username'>
+                    <LinkContainer to='/profile'>
+                      <NavDropdown.Item>Profile</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Item onClick={logoutHandler}>
+                      Logout
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </>
+              ) : (
+                <>
+                  <LinkContainer to='/login'>
+                    <Nav.Link>
+                      <FaSignInAlt /> Sign In
+                    </Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to='/register'>
+                    <Nav.Link>
+                      <FaSignOutAlt /> Sign Up
+                    </Nav.Link>
+                  </LinkContainer>
+                </>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
   );
 };
